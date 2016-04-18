05dd8dcd21a6fa00e3a97433b6156eed02f26c39
gelhen <gelhen@gmail.com>
2016-04-18 09:56:57 -0300

    Update urls.py

--- eventex/urls.py
+++ eventex/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from eventex.core.views import home
-,
+
 urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^inscricao/', include('eventex.subscriptions.urls',
